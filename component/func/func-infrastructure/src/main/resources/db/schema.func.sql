4f2c33c06c216185a20519c16f187c11542c60ef
34009750qianqian <747010136@qq.com>
2022-07-22 13:20:45 +0800

    sql修改，符合条件时引入对应sql

--- component/func/func-infrastructure/src/main/resources/db/schema.func.sql
+++ component/func/func-infrastructure/src/main/resources/db/schema.func.sql
@@ -1,4 +1,4 @@
 
 -- import classpath:db/schema.component_func.sql
 -- import classpath:db/schema.component_func_group.sql
--- import classpath:db/schema.component_user.sql
+-- import classpath:db/schema.component_user.sql condition on class com.particle.user.infrastructure.dos.UserDO
