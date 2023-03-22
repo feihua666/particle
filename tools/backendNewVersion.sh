cdce3e5fbe982b6d8600e9db7fbc12b695bacc26
yangwei <feihua666@sina.com>
2023-03-22 13:24:39 +0800

    更新vuepress版本，所有版本保持一致

--- tools/backendNewVersion.sh
+++ tools/backendNewVersion.sh
@@ -5,4 +5,4 @@ mvn versions:set -DnewVersion=0.0.1-beta
 
 # 请在tools目录下执行该脚本
 # 执行完成后默认会生成 pom.xml.versionsBackup 文件，该文件可用于回退 参见 backendNewVersionRevert.sh
-# 尽量保持前后端版本一致 同步修改 web/project/xxx/package.json 中 version 版本
+# 尽量保持前后端版本一致 同步修改 web/project/xxx/package.json 和 doc/vuepress/package.json 中 version 版本
