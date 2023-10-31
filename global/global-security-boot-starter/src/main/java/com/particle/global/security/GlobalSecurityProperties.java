5ced2f530442f318dbe3d0908bcda61b3a1caad3
yangwei <feihua666@sina.com>
2023-10-31 11:02:16 +0800

    默认session仅支持header解析token

--- global/global-security-boot-starter/src/main/java/com/particle/global/security/GlobalSecurityProperties.java
+++ global/global-security-boot-starter/src/main/java/com/particle/global/security/GlobalSecurityProperties.java
@@ -24,4 +24,11 @@ public class GlobalSecurityProperties {
 	 * 原因：在spring security配置设置了loginPage就默认禁用了默认的登录界面，这样需要自己添加页面，但在开发时有可能只是想用原来的登录界面，这里提供一个配置强制使用原来的，不使用自己定义的
 	 */
 	private Boolean forceNotUseCustomLoginPage = false;
+
+	/**
+	 * 强制在登录成功后在response中写token响应头
+	 * 这在使用cookie和header同时解析sessionId时，如果用户已经登录，但没有退出，重新登录，这时cookie默认是自带发送的，导致在登录时仍能获取后登录用户，在登录成功后响应头中没有token请求头
+	 * 如果单独使用header解析，没有问题，这里设置为false即可，本系统默认设置为false，因为默认使用的是header，参见：application-session.yml
+	 */
+	private Boolean forceWriteLoginHeaderToken = false;
 }
--- global/global-security-boot-starter/src/main/java/com/particle/global/security/security/login/DefaultAuthenticationSuccessHandler.java
+++ global/global-security-boot-starter/src/main/java/com/particle/global/security/security/login/DefaultAuthenticationSuccessHandler.java
@@ -1,10 +1,12 @@
 package com.particle.global.security.security.login;
 
 import cn.hutool.core.io.IoUtil;
+import cn.hutool.core.util.BooleanUtil;
 import cn.hutool.core.util.CharsetUtil;
 import cn.hutool.core.util.StrUtil;
 import cn.hutool.extra.servlet.ServletUtil;
 import com.particle.global.dto.response.SingleResponse;
+import com.particle.global.security.GlobalSecurityProperties;
 import com.particle.global.security.security.ApplicationContextForSecurityHelper;
 import com.particle.global.swagger.SwaggerInfo;
 import com.particle.global.tool.json.JsonTool;
@@ -39,25 +41,30 @@ public class DefaultAuthenticationSuccessHandler extends SavedRequestAwareAuthen
 
     @Autowired(required = false)
     private ServerProperties serverProperties;
+    @Autowired(required = false)
+    private GlobalSecurityProperties globalSecurityProperties;
 
     @Override
     public void onAuthenticationSuccess(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Authentication authentication) throws ServletException, IOException {
 
-        HttpSession session = httpServletRequest.getSession(false);
-        if (session != null) {
-            /**
-             * 设置请求头，这在使用spring session时，目前是使用了header和cookie两个方式，如果在已经登录的情况下重新登录，可能请求头中没有token的情况，这里强制设置一下
-             * 原因是默认在spring commit 时检查了当前session是否有效，如果有效的情况下没有设置，但用户确实已经重新登录了，参见：{@link org.springframework.session.web.http.SessionRepositoryFilter.SessionRepositoryRequestWrapper#commitSession()}
-             * 可通过 server.servlet.session.cookie.name 修改,与{@link com.particle.global.session.auto.GlobalSessionAutoConfiguration#customDelegateHttpSessionIdResolver(ServerProperties, ObjectProvider)} 保持一致的可配置自定义cookie名称的兼容
-             * 注意在{@link  com.particle.global.session.auto.CustomDefaultCookieSerializer} 中也使用了该变量
-             */
-            String tokenName = Optional.ofNullable(serverProperties)
-                    .map(ServerProperties::getServlet)
-                    .map(ServerProperties.Servlet::getSession)
-                    .map(Session::getCookie)
-                    .map(Session.Cookie::getName).orElse(SwaggerInfo.token);
-            httpServletResponse.setHeader(tokenName, session.getId());
+        if (globalSecurityProperties != null && BooleanUtil.isTrue(globalSecurityProperties.getForceWriteLoginHeaderToken())) {
+            HttpSession session = httpServletRequest.getSession(false);
+            if (session != null) {
+                /**
+                 * 设置请求头，这在使用spring session时，目前是使用了header和cookie两个方式，如果在已经登录的情况下重新登录，可能请求头中没有token的情况，这里强制设置一下
+                 * 原因是默认在spring commit 时检查了当前session是否有效，如果有效的情况下没有设置，但用户确实已经重新登录了，参见：{@link org.springframework.session.web.http.SessionRepositoryFilter.SessionRepositoryRequestWrapper#commitSession()}
+                 * 可通过 server.servlet.session.cookie.name 修改,与{@link com.particle.global.session.auto.GlobalSessionAutoConfiguration#customDelegateHttpSessionIdResolver(ServerProperties, ObjectProvider)} 保持一致的可配置自定义cookie名称的兼容
+                 * 注意在{@link  com.particle.global.session.auto.CustomDefaultCookieSerializer} 中也使用了该变量
+                 */
+                String tokenName = Optional.ofNullable(serverProperties)
+                        .map(ServerProperties::getServlet)
+                        .map(ServerProperties.Servlet::getSession)
+                        .map(Session::getCookie)
+                        .map(Session.Cookie::getName).orElse(SwaggerInfo.token);
+                httpServletResponse.setHeader(tokenName, session.getId());
+            }
         }
+
 
         Object principal = authentication.getPrincipal();
         if (principal instanceof LoginUser) {
