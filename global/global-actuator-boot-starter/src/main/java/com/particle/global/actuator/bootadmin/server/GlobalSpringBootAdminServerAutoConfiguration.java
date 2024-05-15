04311aa4eea527767be84d8b908642998d3c4ce6
yangwei <feihua666@sina.com>
2024-05-15 11:42:18 +0800

    修改bootadmin注释，主要添加怎么禁用，因为在单机部署时如果在依赖包的情况下需要禁用

--- global/global-actuator-boot-starter/src/main/java/com/particle/global/actuator/bootadmin/server/GlobalSpringBootAdminServerAutoConfiguration.java
+++ global/global-actuator-boot-starter/src/main/java/com/particle/global/actuator/bootadmin/server/GlobalSpringBootAdminServerAutoConfiguration.java
@@ -1,14 +1,21 @@
 package com.particle.global.actuator.bootadmin.server;
 
+import de.codecentric.boot.admin.client.config.SpringBootAdminClientEnabledCondition;
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import de.codecentric.boot.admin.server.config.SpringBootAdminServerEnabledCondition;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.ConditionContext;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.type.AnnotatedTypeMetadata;
 
 /**
  * <p>
  * spring boot admin server 自动配置类
+ * server 可以使用 -Dspring.boot.admin.server.enabled=false 关闭，参见 {@link SpringBootAdminServerEnabledCondition#getMatchOutcome(ConditionContext, AnnotatedTypeMetadata)}
+ * server 同时也可以使用 -Dparticle.actuator.bootadmin.server.enabled=false 关闭,因为 {@link EnableAdminServer} 会不生效
+ * client 可以使用 -Dspring.boot.admin.client.enabled=false 关闭，参见 {@link SpringBootAdminClientEnabledCondition#getMatchOutcome(ConditionContext, AnnotatedTypeMetadata)}
  * </p>
  *
  * @author yangwei
@@ -19,4 +26,7 @@ import org.springframework.context.annotation.Configuration;
 @ConditionalOnClass(EnableAdminServer.class)
 @ConditionalOnProperty(prefix = "particle.actuator.bootadmin.server", name = "enabled", havingValue = "true", matchIfMissing = true)
 public class GlobalSpringBootAdminServerAutoConfiguration {
+    public GlobalSpringBootAdminServerAutoConfiguration() {
+        System.out.println("GlobalSpringBootAdminServerAutoConfiguration");
+    }
 }
