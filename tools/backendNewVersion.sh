badaf23a3d96d4b46c029c8c645321acf555fb32
yangwei <feihua666@sina.com>
2024-12-06 18:01:23 +0800

    修改版本为 6.0.1-beta-SNAPSHOT，从该版本开始将升级jdk为java17和spring boot 3.0以上

--- tools/backendNewVersion.sh
+++ tools/backendNewVersion.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 cd ../
 pwd
-mvn versions:set -DnewVersion=4.0.1-beta-SNAPSHOT
+mvn versions:set -DnewVersion=6.0.1-beta-SNAPSHOT
 
 # 请在tools目录下执行该脚本
 # 执行完成后默认会生成 pom.xml.versionsBackup 文件，该文件可用于回退 参见 backendNewVersionRevert.sh
 # 尽量保持前后端版本一致 同步修改 web/project/xxx/package.json 和 doc/vuepress/package.json 中 version 版本
 
 # 如果是全局替换版本,按以下步骤比较精准
-# 1. pom.xml <version>4.0.1-beta-SNAPSHOT</version> 替换为 <version>新版本</version>
-# 2. package.json "version": "4.0.1-beta-SNAPSHOT", 替换为 "version": "新版本",
+# 1. pom.xml <version>6.0.1-beta-SNAPSHOT</version> 替换为 <version>新版本</version>
+# 2. package.json "version": "6.0.1-beta-SNAPSHOT", 替换为 "version": "新版本",
